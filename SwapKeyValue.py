f2874d002aa39a0dd9a59dc6c4144d489582ab10
Ankita Yadav <62417515+ankita-y@users.noreply.github.com>
2021-06-21 23:09:29 +0530

    Swap key and value in Dictionary

---
+++ SwapKeyValue.py
@@ -0,0 +1,22 @@
+# Swap the key and value in Dictionary
+payload = {'elevan':[11],'twelve':[12,13,14],'thirteen':[15,16],'four':[15,16]}
+new_payload = {}
+for key,val in payload.items():
+    val = tuple(val)
+    if val not in new_payload:
+        new_payload[val] = [key]
+    else:
+        new_payload[val].append(key)
+        
+print(new_payload)
+
+# Output
+# {(11,): ['elevan'], (12, 13, 14): ['twelve'], (15, 16): ['thirteen', 'four']}
+# {(11,): 'elevan', (12, 13, 14): 'twelve', (15, 16): 'thirteen'}
+
+payload = {'a':97,'b':98,'c':99,'d':100}
+newpayload = {val:key for key,val in payload.items()}
+print(newpayload)
+
+# Output
+# {97: 'a', 98: 'b', 99: 'c', 100: 'd'}
