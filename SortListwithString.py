ef049f6f071c931eb3e8b839a0fffe0d3beff6c1
Ankita Yadav <62417515+ankita-y@users.noreply.github.com>
2023-05-15 00:37:07 +0530

    Update SortListwithString.py

--- SortListwithString.py
+++ SortListwithString.py
@@ -1,3 +1,5 @@
+# Sort the string according to order_arr
+
 name_arr = ["Vidya", "Deepika","Monika","Sachin"]
 order_arr = ["M","V","D","S"]
 sortarr = []
@@ -6,5 +8,8 @@ for i in range(len(name_arr)):
       
         if name_arr[j][0] == order_arr[i]:
             sortarr.append(name_arr[j])
-            
+# for arr1 in order_arr:
+#     for arr2 in name_arr:
+#         if arr1 == arr2[0]:
+#             sortarr.append(arr2)            
 print(sortarr)
