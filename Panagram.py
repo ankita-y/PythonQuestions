154eb2c67144414e403431339cd34c812c61a99c
Ankita Yadav <62417515+ankita-y@users.noreply.github.com>
2023-05-15 00:18:33 +0530

    Update Panagram.py

--- Panagram.py
+++ Panagram.py
@@ -1,14 +1,23 @@
 # str = "The quick brown fox jumps over the lazy dog"
-str = input()
+string = input()
 # to remove the space from the string and convert it into lower case
-str ="".join([c for c in str if c != " "]).lower()
+string ="".join([c for c in string if c != " "]).lower()
 index = 0
 
 for i in range(len(str)):
-    if 'a' <= str[i] <= 'z':
+    if 'a' <= string[i] <= 'z':
         index += 1
 
 if index >= 26:
     print("The String is a Pangram String.")
 else:
     print("The String is not a Pangram String.")
+# Method 2
+string = string.replace(' ','').lower()
+newstr = set()
+for i in range(len(string)): newstr.add(string[i])
+
+if len(newstr) == 26:
+    print("The string is panagram string")
+else:
+    print("The string is not panagram string")
